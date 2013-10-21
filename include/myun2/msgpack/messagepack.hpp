934ebce90734764322f5e2254fcb8d1ca58cb547
myun2 <myun2@nwhite.info>
2013-10-21 16:59:22 +0900

    messagepack Class

--- include/myun2/msgpack/messagepack.hpp
+++ include/myun2/msgpack/messagepack.hpp
@@ -1,4 +1,11 @@
 #ifndef __github_com_myun2__messagepack_HPP__
 #define __github_com_myun2__messagepack_HPP__
 
+namespace myun2
+{
+	class messagepack
+	{
+	};
+}
+
 #endif//__github_com_myun2__messagepack_HPP__
