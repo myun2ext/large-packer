7bfe7617b0509564e2d02c2763fb29c46ff2e328
myun2 <myun2@nwhite.info>
2013-10-21 17:21:00 +0900

    yaml/structure.hpp Add

---
+++ include/myun2/yaml/structure.hpp
@@ -0,0 +1,11 @@
+#ifndef __github_com_myun2__yaml__structure_HPP__
+#define __github_com_myun2__yaml__structure_HPP__
+
+namespace myun2
+{
+	namespace yaml
+	{
+	}
+}
+
+#endif//__github_com_myun2__yaml__structure_HPP__
