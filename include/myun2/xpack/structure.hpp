88c12c5ad766bef47c36eac2b6fe08716d631d9a
myun2 <myun2@nwhite.info>
2013-10-22 15:45:54 +0900

    myun2/xpack/structure.hpp Add

---
+++ include/myun2/xpack/structure.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__xpack__structure_HPP__
+#define __github_com_myun2__xpack__structure_HPP__
+
+namespace myun2
+{
+	namespace xpack
+	{
+		struct structure
+		{
+		};
+	}
+}
+
+#endif//__github_com_myun2__xpack__structure_HPP__
