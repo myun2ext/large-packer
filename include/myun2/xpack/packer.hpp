dd8e27f2ff8817ad7e0d860e702cecba23a11745
myun2 <myun2@nwhite.info>
2013-10-23 12:51:57 +0900

    [xpack] packer/depacker Add

---
+++ include/myun2/xpack/packer.hpp
@@ -0,0 +1,11 @@
+#ifndef __github_com_myun2__xpack__packer_HPP__
+#define __github_com_myun2__xpack__packer_HPP__
+
+namespace myun2
+{
+	namespace xpack
+	{
+	}
+}
+
+#endif//__github_com_myun2__xpack__packer_HPP__
---
+++ include/myun2/xpack/unpacker.hpp
@@ -0,0 +1,11 @@
+#ifndef __github_com_myun2__xpack__unpacker_HPP__
+#define __github_com_myun2__xpack__unpacker_HPP__
+
+namespace myun2
+{
+	namespace xpack
+	{
+	}
+}
+
+#endif//__github_com_myun2__xpack__unpacker_HPP__
