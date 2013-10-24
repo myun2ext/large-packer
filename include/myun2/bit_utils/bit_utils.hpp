713990f7603670281dd45df447b927d13d8eab76
myun2 <myun2@nwhite.info>
2013-10-24 15:03:04 +0900

    bit_utils.hpp: fill_higher_bits

--- include/myun2/bit_utils/bit_utils.hpp
+++ include/myun2/bit_utils/bit_utils.hpp
@@ -8,6 +8,13 @@ namespace myun2
 		template <typename T> T zero() { return T(0); }
 		template <typename T> T reverse(const T& in) { return ~in; }
 		template <typename T> T filled() { return reverse(zero()); }
+		template <typename T> T type_of_bits() { return sizeof(T) * 8; }
+		template <typename T> T bits() { return sizeof(T) * 8; }
+
+		////////////////
+
+		template <typename T> T fill_higher_bits(unsigned int count) { return fille() << (bits() - count); }
+		template <typename T> T fill_higher_bits(unsigned int count, const T& target) { return fill_higher_bits(count) || target; }
 	}
 }
 
