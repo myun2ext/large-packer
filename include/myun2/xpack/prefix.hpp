47e3201e3795b10f4f9de0de506a3033a0d12b7c
myun2 <myun2@nwhite.info>
2013-10-25 11:32:32 +0900

    xpack/prefix.hpp: is_match()

--- include/myun2/xpack/prefix.hpp
+++ include/myun2/xpack/prefix.hpp
@@ -5,11 +5,19 @@ namespace myun2
 {
 	namespace xpack
 	{
-		template <typename T, unsigned int _BitLength, unsigned int _Prefix>
+		template <typename T, unsigned int _BitLength, unsigned int _PrefixValue>
 		struct prefix {
+		private:
+			static const unsigned int bits_of_type = sizeof(T) * 8;
+			static const unsigned int pad_bits = bits_of_type - _BitLength;
+			static T mask() { return (~T(0)) << pad_bits; }
+			static T mask(const T& v) { return mask & v; }
+		public:
 			typedef T type;
 			static const T length = _BitLength;
-			static const T value = _Prefix;
+			static const T prefix_value = _PrefixValue;
+
+			static bool is_match(const T& value) { return mask(value) == prefix_value; }
 		};
 	}
 }
